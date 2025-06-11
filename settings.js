ef2cc35d022c1a7e183166309f066f71f3cbb0c5
Rkamrul <mrkamrulhasan02@gmail.com>
2025-06-11 21:19:59 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Mr KHF Bot",
+  botOwner: 'Kamrul Hasan', // Your name
+  ownerNumber: '8801858609070', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
